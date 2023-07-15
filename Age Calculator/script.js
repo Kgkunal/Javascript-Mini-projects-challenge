ef5b4ab9a798a9a0a86cc7b0bb326c05f989bac9
Kunal D Gavhane <kgkunalgavhane1721@gmail.com>
2023-07-15 22:16:46 +0530

    js script for Age Calculator Raedy

---
+++ Age Calculator/script.js
@@ -0,0 +1,76 @@
+const AgeCalculate = () => {
+    const today = new Date();
+    const inputDate = new Date(document.getElementById("date-input").value);
+
+    const birthDetails = {
+        date: inputDate.getDate(),
+        month: inputDate.getMonth() + 1,
+        year: inputDate.getFullYear(),
+    };
+
+    const currentYear = today.getFullYear();
+    const currentMonth = today.getMonth();
+    const currentDate = today.getDate();
+
+    if (isFutureDate(birthDetails, currentDate, currentMonth, currentYear)) {
+        alert("Person with this Birthdate not born yet");
+        displayResult("-", "-", "-");
+        return;
+    }
+
+    const { years, months, days } = calculateAge(
+        birthDetails,
+        currentDate,
+        currentMonth,
+        currentYear
+    );
+
+    displayResult(years, months, days);
+};
+
+const isFutureDate = (birthDetails, currentDate, currentMonth, currentYear) => {
+    return (
+        birthDetails.year > currentYear ||
+        (birthDetails.year === currentYear &&
+            (birthDetails.month > currentMonth ||
+                (birthDetails.month === currentMonth && birthDetails.date > currentDate)))
+    );
+};
+
+const calculateAge = (birthDetails, currentDate, currentMonth, currentYear) => {
+    let years = currentYear - birthDetails.year;
+    let months, days;
+
+    if (currentMonth < birthDetails.month) {
+        years--;
+        months = 12 - (birthDetails.month - currentMonth);
+    } else {
+        months = currentMonth - birthDetails.month;
+    }
+
+    if (currentDate < birthDetails.date) {
+        months--;
+        const lastMonth = currentMonth === 0 ? 11 : currentMonth - 1;
+
+        const daysInLastMonth = getDaysInMonth(lastMonth, currentYear);
+        days = daysInLastMonth - (birthDetails.date - currentDate);
+    } else {
+        days = currentDate - birthDetails.date;
+    }
+
+    return { years, months, days };
+};
+
+const getDaysInMonth = (month, year) => {
+    const isLeapYear = year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+    const daysInMonth = [31, isLeapYear ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    return daysInMonth[month - 1];
+};
+
+const displayResult = (bdate, bMonth, bYear) => {
+    document.getElementById("years").textContent = bYear;
+    document.getElementById("months").textContent = bMonth;
+    document.getElementById("days").textContent = bdate;
+};
+
+document.getElementById("calc-age-btn").addEventListener("click", AgeCalculate);
